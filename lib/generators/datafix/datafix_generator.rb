710fb008d66ff7d7bc1cada02ce03ca0e153e8f6
Michael Gamba & Ryan Ong <pair+mike+rong@pivotallabs.com>
2012-11-08 11:46:04 -0500

    Add guards/warnings for spec templates

--- lib/generators/datafix/datafix_generator.rb
+++ lib/generators/datafix/datafix_generator.rb
@@ -6,7 +6,12 @@ class DatafixGenerator < Rails::Generators::NamedBase
     raise "Datafix with name '#{file_name}' already exists" if Dir.glob("#{PATH}/*_#{file_name}.rb").any?
 
     template "datafix_template.rb.erb", "#{PATH}/#{timestamp}_#{file_name}.rb"
-    template "datafix_spec_template.rb.erb", "spec/#{PATH}/#{timestamp}_#{file_name}_spec.rb"
+
+    if Rails.application.config.generators.rails[:test_framework] == :rspec
+      template "datafix_spec_template.rb.erb", "spec/#{PATH}/#{timestamp}_#{file_name}_spec.rb"
+    else
+      puts "Datafix test was not generated. Datafix requires rspec."
+    end
   end
 
   def timestamp
